43fdecb4ec5f7d084920c133c49b180cf1a3c8ed
Laxmisrii K <luxmisrikarunakaran03@gmail.com>
2026-04-03 22:00:15 +0530

    backend deploy

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import smtplib
 from email.mime.text import MIMEText
+import os
 
 app = Flask(__name__)
 CORS(app)
 
-# ⚠️ Replace with your email & app password
-EMAIL = "laxmisrii@gmail.com"
-PASSWORD = "mzfsgcbhtjbwmiyj"
+EMAIL = os.environ.get("EMAIL")
+PASSWORD = os.environ.get("PASSWORD")
 
 @app.route('/send', methods=['POST'])
 def send_email():
@@ -19,7 +19,7 @@ def send_email():
 
     try:
         msg = MIMEText(f"Name: {name}\nEmail: {email}\n\nMessage:\n{message}")
-        msg['Subject'] = "New Portfolio Message"
+        msg['Subject'] = "Portfolio Contact"
         msg['From'] = EMAIL
         msg['To'] = EMAIL
 
@@ -32,6 +32,6 @@ def send_email():
     except Exception as e:
         print(e)
         return jsonify({"status": "error"})
-    
+
 if __name__ == "__main__":
-    app.run(port=8001, debug=True)
+    app.run(host='0.0.0.0', port=8001)
